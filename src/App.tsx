1772fb594a78d690f6e4adb8f82f35b0cf849762
Maniii97 <mjha34499@gmail.com>
2024-08-13 02:20:47 +0530

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/header/Header'; 
 import FlipCard from './components/card/Card'; 
 import Dashboard from './pages/Dashboard';
